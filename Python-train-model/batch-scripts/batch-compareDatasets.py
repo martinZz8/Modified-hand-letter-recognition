79ccea1c4005e5ebdfb7e44cc87617835045d707
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2023-12-29 00:36:39 +0100

    started creating batch script to compare datasets
    OpenPose v1 vs v2 (where are changes in input skeletons)
    also reparied "loadData" function

---
+++ Python-train-model/batch-scripts/batch-compareDatasets.py
@@ -0,0 +1,11 @@
+from os import listdir
+from os.path import join, isfile
+
+# TODO - finish this batch script
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    print("-- END OF SCRIPT --")
