27ac8720eede40027e3a8d598b601f68dcca8285
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2024-01-27 16:13:48 +0100

    up last commit from multi-tests branch

--- Python-train-model/functions/getArgumentOptions.py
+++ Python-train-model/functions/getArgumentOptions.py
@@ -2,11 +2,18 @@ import getopt
 import sys
 
 
+# Custom exception declaration (only used and caught in this file)
+class WrongLOSOPersonInteger(Exception):
+    pass
+
+
 def getArgumentOptions(argv,
                        useMediaPipe: bool,
                        useShiftedData: bool,
+                       losoPersonTester: int,
                        useCuda: bool,
-                       modelRepeats: int):
+                       modelRepeats: int,
+                       showGraphs: bool):
     # --Read input arguments and set variables--
     # Note:
     # - each option consists of key and value pair
@@ -15,9 +22,9 @@ def getArgumentOptions(argv,
 
     # Also note, that in second argument of "getopt.getopt()" method (short args) you should provice colon ':' after short argument, if it's with value, otherwise no.
     # Same thing goes to third argument (long args), but with equal sign '='.
-    opts, args = getopt.getopt(argv, "hmosScCr:",
-                               ["help", "media-pipe", "open-pose", "shifted-data", "no-shifted-data", "cuda", "cpu",
-                                "model-repeats="])
+    opts, args = getopt.getopt(argv, "hmosSl:cCr:gG",
+                               ["help", "media-pipe", "open-pose", "shifted-data", "no-shifted-data", "loso-person-tester=", "cuda", "cpu",
+                                "model-repeats=", "show-graphs", "hide-graphs"])
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):  # help
@@ -27,13 +34,17 @@ def getArgumentOptions(argv,
                   '-o, --open-pose (use OpenPose)\n'
                   '-s, --shifted-data (use shifted data - default)\n'
                   '-S, --no-shifted-data (use non-shifted data)\n'
+                  '-l, --loso-person-tester (specify, which person has to be LOSO tester, other persons are used to train model - default -1, which stands for not using LOSO dataset)\n'
                   '-c, --cuda (use cuda if available - default)\n'
                   '-C, --cpu (use cpu)'
-                  '-r, --model-repeats (number of model repeats, gets only best model based in train and test acc - default 1)\n\n'
+                  '-r, --model-repeats (number of model repeats, gets only best model based in train and test acc - default 1)\n'
+                  '-g, --show-graphs (show best model loss and accuracy graphs at the end of model training - default)\n'
+                  '-G, --hide-graphs (hide best model loss and accuracy graphs at the end of model training)\n\n'
                   'Also note, that you should use only one of the following pair values (otherwise it would be used the least provided):\n'
                   '-m, -o\n'
                   '-s, -S\n'
-                  '-c, -C')
+                  '-c, -C\n',
+                  '-g, -G')
             sys.exit()
         elif opt in ("-m", "--media-pipe"):  # use MediaPipe data - default
             useMediaPipe = True
@@ -43,10 +54,31 @@ def getArgumentOptions(argv,
             useShiftedData = True
         elif opt in ("-S", "--no-shifted-data"):  # use non-shifted data
             useShiftedData = False
+        elif opt in ("-l", "--loso-person-tester"):  # specify, which person has to be LOSO tester, other persons are used to train model - default -1, which stands for not using LOSO dataset
+            # Check if "arg" string has integer representation of value (that could be casted to int)
+            doesThrowWrongNumberException = False
+            try:
+                if arg.isnumeric():
+                    intArg = int(arg)
+                    if intArg >= -1:
+                        losoPersonTester = intArg
+                    else:
+                        doesThrowWrongNumberException = True
+                else:
+                    doesThrowWrongNumberException = True
+
+                if doesThrowWrongNumberException:
+                    raise WrongLOSOPersonInteger()
+            except WrongLOSOPersonInteger:
+                raise Exception("'-l' or '--loso-person-tester' parameter can have argument only of type integer (from -1 to infinity)")
         elif opt in ("-c", "--cuda"):  # use cuda if available - default
             useCuda = True
-        elif opt in ("-C", "--cpu"):  # use cpu
+        elif opt in ("-C", "--show-graphs"):  # use cpu
             useCuda = False
+        elif opt in ("-g", "--hide-graphs"):  # show best model loss and accuracy graphs at the end of model training - default
+            showGraphs = True
+        elif opt in ("-G", "--cpu"):  # hide best model loss and accuracy graphs at the end of model training
+            showGraphs = False
         elif opt in ("-r", "--model-repeats"):  # set modelRepeats value - default 1
             # Check if "arg" string has integer representation of value (that could be casted to int)
             if arg.isnumeric():
@@ -57,7 +89,9 @@ def getArgumentOptions(argv,
     print(f"Used options:\n"
           f"- useMediaPipe = {useMediaPipe}\n"
           f"- useShiftedData = {useShiftedData}\n"
+          f"- losoPersonTester = {losoPersonTester}{'(don''t use LOSO)' if losoPersonTester <= -1 else ''}\n"
           f"- useCuda = {useCuda}\n"
-          f"- modelRepeats = {modelRepeats}\n")
+          f"- modelRepeats = {modelRepeats}\n",
+          f"- showGraphs = {showGraphs}\n")
 
-    return useMediaPipe, useShiftedData, useCuda, modelRepeats
+    return useMediaPipe, useShiftedData, losoPersonTester, useCuda, modelRepeats, showGraphs
