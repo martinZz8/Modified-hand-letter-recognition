64d6b33f2dd12bd849b2f8159a94d8a2eb3d4b9b
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2024-02-01 01:17:54 +0100

    adjusted LOSO test in consts and single classify script

--- Python-eval-all/single-classify/functions/getArgumentOptionsSingle.py
+++ Python-eval-all/single-classify/functions/getArgumentOptionsSingle.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import getopt
 import sys
 
@@ -5,6 +6,7 @@ import sys
 def getArgumentOptionsSingle(argv,
                              useMediaPipe: bool,
                              modelVersion: int,
+                             losoModelPerson: Optional[int],
                              useMatlabPreprocessing: bool,
                              useShiftedData: bool,
                              inputFolderPath: str,
@@ -20,8 +22,8 @@ def getArgumentOptionsSingle(argv,
 
     # Also note, that in second argument of "getopt.getopt()" method (short args) you should provice colon ':' after short argument, if it's with value, otherwise no.
     # Same thing goes to third argument (long args), but with equal sign '='.
-    opts, args = getopt.getopt(argv, "hmov:pPsSf:i:t:rRcC",
-                               ["help", "media-pipe", "open-pose", "--model-version=", "preprocessing", "no-preprocessing", "shifted-data", "no-shifted-data", "input-folder-path=", "input-image=", "output-file=", "image-resize", "skeleton-rescale", "cuda", "cpu"])
+    opts, args = getopt.getopt(argv, "hmov:l:pPsSf:i:t:rRcC",
+                               ["help", "media-pipe", "open-pose", "--model-version=", "loso-model-person=", "preprocessing", "no-preprocessing", "shifted-data", "no-shifted-data", "input-folder-path=", "input-image=", "output-file=", "image-resize", "skeleton-rescale", "cuda", "cpu"])
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):  # help
@@ -30,6 +32,7 @@ def getArgumentOptionsSingle(argv,
                   '-m, --media-pipe (use MediaPipe - default)\n'
                   '-o, --open-pose (use OpenPose)\n'
                   '-v, --model-version (specify version of model - default 1)\n'
+                  '-l, --loso-model-person (specify LOSO model version - default None)\n'
                   '-p, --preprocessing (use Matlab preprocessing, skeleton transformation script - default)\n'
                   '-P, --no-preprocessing (do not use Matlab preprocessing, skeleton transformation script)\n'
                   '-s, --shifted-data (use shifted data - default)\n'
@@ -59,6 +62,13 @@ def getArgumentOptionsSingle(argv,
                 modelVersion = int(arg)
             else:
                 raise Exception("'-v' or '--model-version' parameter can have argument only of type integer")
+        elif opt in ("-l", "--loso-model-person"):  # specify LOSO model version - default None
+            # Check if "arg" string has integer representation of value (that could be casted to int)
+            arg = arg.strip()  # necessary strip to remove leading and trailing spaces
+            if arg.isnumeric():
+                losoModelPerson = int(arg)
+            else:
+                raise Exception("'-l' or '--loso-model-person' parameter can have argument only of type integer")
         elif opt in ("-o", "--open-pose"):  # use OpenPose data
             useMediaPipe = False
         elif opt in ("-p", "--preprocessing"):  # use Matlab preprocessing, skeleton transformation script - default
@@ -91,6 +101,7 @@ def getArgumentOptionsSingle(argv,
     print(f"Used options:\n"
           f"- useMediaPipe = {useMediaPipe}\n"
           f"- modelVersion = {modelVersion}\n"
+          f"- losoModelPerson = {losoModelPerson}\n"
           f"- useMatlabPreprocessing = {useMatlabPreprocessing}\n"
           f"- useShiftedData = {useShiftedData}\n"
           f"- inputFolderPath = {inputFolderPath}\n"
@@ -99,4 +110,4 @@ def getArgumentOptionsSingle(argv,
           f"- useImageResize = {useImageResize}\n"
           f"- useCuda = {useCuda}\n")
 
-    return useMediaPipe, modelVersion, useMatlabPreprocessing, useShiftedData, inputFolderPath, inputImageName, outputFileName, useImageResize, useCuda
+    return useMediaPipe, modelVersion, losoModelPerson, useMatlabPreprocessing, useShiftedData, inputFolderPath, inputImageName, outputFileName, useImageResize, useCuda
